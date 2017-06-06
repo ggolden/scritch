6ad6646500680c74f3cea2815504a71cd1407603
Glenn R. Golden <ggolden@etudes.org>
2017-06-06 09:11:11 -0400

    Initial Commit

---
+++ skritch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  skritch
+//
+//  Created by Glenn R. Golden on 6/6/17.
+//  Copyright © 2017 Glenn R. Golden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
